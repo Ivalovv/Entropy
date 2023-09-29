53d400bff45f22d4879f31a4f9ff282249c99e78
Ivalov <68162833+Ivalovv@users.noreply.github.com>
2023-09-30 00:50:23 +0300

    updat

--- Form1.cs
+++ Form1.cs
@@ -15,56 +15,85 @@ namespace Entropy
     {
         private string text;
 
-        private double entropy;
-
         private char[] alphabet;
 
-        private double[] letterFrequency = new double[34];
+        private Dictionary<string, double> frequency1 = new Dictionary<string, double>();
+        private Dictionary<string, double> frequency2 = new Dictionary<string, double>();
+        private Dictionary<string, double> frequency3 = new Dictionary<string, double>();
 
         public Form1()
         {
             InitializeComponent();
 
             alphabet = new char[34] { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ' };
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                frequency1.Add(alphabet[i].ToString(), 0.0f);
+            }
+
+            for (int i = 0; i < alphabet.Length - 1; i++)
+            {
+                for (int j = 0; j < alphabet.Length - 1; j++)
+                {
+                    frequency2.Add(alphabet[i].ToString() + alphabet[j].ToString(), 0.0f);
+                }
+            }
+
+            for (int i = 0; i < alphabet.Length - 1; i++)
+            {
+                for (int j = 0; j < alphabet.Length - 1; j++)
+                {
+                    for (int k = 0; k < alphabet.Length - 1; k++)
+                    {
+                        frequency3.Add(alphabet[i].ToString() + alphabet[j].ToString() + alphabet[k].ToString(), 0.0f);
+                    }
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double numberLetters = 0;
-
-            for (int i = 0; i < letterFrequency.Length; i++) letterFrequency[i] = 0.0d;
-
-            textBoxEn.Text = string.Empty;
+            textBoxEn1.Text = string.Empty;
             text = textBox1.Text;
 
-            for (int i = 0; i < text.Length; i++)
+            textBoxEn1.Text = entropyCalculation(ref frequency1, ref text).ToString();
+            textBoxEn2.Text = entropyCalculation(ref frequency2, ref text).ToString();
+            textBoxEn3.Text = entropyCalculation(ref frequency3, ref text).ToString();
+        }
+
+        private double entropyCalculation(ref Dictionary<string, double> frequency, ref string text)
+        {
+            double numberLetters = 0;
+            double entropy = 0;
+
+            int keyLen = frequency.Keys.First().Length;
+
+            foreach (string key in frequency.Keys.ToList()) 
             {
-                for (int j = 0; j < alphabet.Length; j++)
+                frequency[key] = 0.0f;
+            }
+
+            for (int i = 0; i < (text.Length - (keyLen - 1)); i++)
+            {
+                if (frequency.ContainsKey(text.Substring(i, keyLen).ToString()))
                 {
-                    if (text[i] == alphabet[j])
-                    {
-                        letterFrequency[j] += 1;
-                        break;
-                    }
+                    frequency[text.Substring(i, keyLen).ToString()]++;
+                    numberLetters++;
                 }
             }
 
-            for (int i = 0; i < letterFrequency.Length; i++)
+            foreach (string key in frequency.Keys.ToList())
             {
-                numberLetters += letterFrequency[i];
+                frequency[key] /= numberLetters;
+
+                if (frequency[key] != 0)
+                {
+                    entropy += frequency[key] * Math.Log(1.0f / frequency[key], 2);
+                }
             }
 
-            for (int i = 0; i < letterFrequency.Length; i++)
-            {
-                letterFrequency[i] = letterFrequency[i] / numberLetters;
-            }
-
-            for (int i = 0; i < letterFrequency.Length; i++)
-            {
-                entropy = letterFrequency[i] * Math.Log(1 / letterFrequency[i], 2);
-            }
-
-            textBoxEn.Text = entropy.ToString();
+            return entropy;
         }
     }
 }
